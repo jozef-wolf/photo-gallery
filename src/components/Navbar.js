5adabc9fa02d7f5474155a6c51bfd1abf5047650
jozef-wolf <jozefwolf@o2.pl>
2021-03-15 14:21:26 +0100

    navbar wip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 const Navbar = () => {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__menu">menu</div>
+      <div className="navbar__title">Józef Wolf</div>
+      <div className="navbar__contact">Kotakt</div>
+    </div>
+  );
 };
 
 export default Navbar;

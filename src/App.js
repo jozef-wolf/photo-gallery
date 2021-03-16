7f1a2c1eca4484cd549bae98a267511a8688820e
jozef-wolf <jozefwolf@o2.pl>
2021-03-16 13:53:52 +0100

    sass set and icon removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./App.css";
+import "./css/main.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">

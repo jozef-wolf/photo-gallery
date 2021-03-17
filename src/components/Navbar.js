aa977ade46ce3662ca32f67af3a01a8e33ea9cf3
jozef-wolf <jozefwolf@o2.pl>
2021-03-17 09:51:35 +0100

    react hamburger icon imported

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  return <nav>the nav</nav>;
+  return (
+    <nav>
+      <FontAwesomeIcon icon={faBars} />
+    </nav>
+  );
 };
 
 export default Navbar;

c4d686e7fb3cb4047660cb193472c59257bfe59b
jozef-wolf <jozefwolf@o2.pl>
2021-03-17 15:51:12 +0100

    first section added with scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./css/main.css";
 import Header from "./components/Header";
+import FirstSection from "./components/FirstSection";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FirstSection />
     </div>
   );
 }

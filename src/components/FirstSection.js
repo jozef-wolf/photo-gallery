383c53551c972b4d728a1ac96f9e8ef8cb9d625a
jozef-wolf <jozefwolf@o2.pl>
2021-03-18 10:03:32 +0100

    img added

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -1,9 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import photo from "../img/india-train.jpg";
 
 const FirstSection = () => {
   return (
     <div className="section">
+      <img src={photo} alt="" />
       <FontAwesomeIcon className="arrow--down" icon={faAngleDown} />
     </div>
   );

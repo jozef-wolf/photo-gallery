65254138a1c97a075ac14ff61433e920a216cb24
jozef-wolf <wolf.joozef@gmail.com>
2021-04-30 13:36:11 +0200

    navbar left and right

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./css/main.css";
-
+import Navbar from "./components/navbar/Navbar";
 import FirstSection from "./components/FirstSection";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <FirstSection />
     </div>
   );
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="left-side"></div>
+      <div className="right-side"></div>
+    </div>
+  );
+};
+
+export default Navbar;

e1d26a1764c9d0e96b3dcc83ad3b6f8342e2e284
jozef-wolf <wolf.joozef@gmail.com>
2021-04-30 14:00:20 +0200

    navbar links added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <div className="left-side"></div>
+      <div className="left-side">
+        <div className="links">
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
       <div className="right-side"></div>
     </div>
   );

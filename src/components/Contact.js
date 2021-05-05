e2f070d6ecab2b9b7c03d8a50efff03ab8146748
jozef-wolf <wolf.joozef@gmail.com>
2021-05-05 13:59:35 +0200

    contact filled

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,8 @@ const Contact = () => {
   return (
     <div className="contact">
       <h2>Contact</h2>
+      <a href="mailto: wolf.joozef@gmail.com">wolf.joozef@gmail.com</a>
+      <a href="tel:+48509853644">+48 509 853 644</a>
     </div>
   );
 };

b16441b661e1fa7b88264fef323b34ac0ad7ed37
jozef-wolf <wolf.joozef@gmail.com>
2021-05-05 11:37:29 +0200

    react router working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./css/main.css";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import photo from "../img/india-train.jpg";
 
 const Home = () => {
   return (
     <div className="home">
       <img src={photo} alt="" />
-      <FontAwesomeIcon className="arrow--down" icon={faAngleDown} />
     </div>
   );
 };

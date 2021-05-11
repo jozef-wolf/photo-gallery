2ca78517d647986f4de0c7b084400f5181a74ae4
jozef-wolf <wolf.joozef@gmail.com>
2021-05-11 12:14:51 +0200

    img hardcoded to photos.js

---
+++ src/components/photos/Astrophoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Astrophoto = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Astrophoto
--- src/components/photos/Photos.js
+++ src/components/photos/Photos.js
@@ -1,24 +1,28 @@
 import React from "react";
+import Astrophoto from "../../img/astrophoto.jpg";
+import Events from "../../img/events.jpg";
+import Travel from "../../img/travel.jpg";
+import Property from "../../img/property.jpg";
 
 const Photos = () => {
   return (
     <div className="photos">
       <h2>Photos</h2>
-      <div class="container">
+      <div className="container">
         <div>
-          <img src="" alt=""></img>
+          <img src={Travel} alt=""></img>
           <h3>Travel</h3>
         </div>
         <div>
-          <img src="" alt=""></img>
+          <img src={Astrophoto} alt=""></img>
           <h3>Astrophoto</h3>
         </div>
         <div>
-          <img src="" alt=""></img>
+          <img src={Property} alt=""></img>
           <h3>Property</h3>
         </div>
         <div>
-          <img src="" alt=""></img>
+          <img src={Events} alt=""></img>
           <h3>Events</h3>
         </div>
       </div>

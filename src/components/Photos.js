253879336eaf4c31645ce9c820f3e5ef6836b6a5
jozef-wolf <wolf.joozef@gmail.com>
2021-05-11 11:19:14 +0200

    photos section wip

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -4,6 +4,24 @@ const Photos = () => {
   return (
     <div className="photos">
       <h2>Photos</h2>
+      <div class="container">
+        <div>
+          <img></img>
+          <h3>Travel</h3>
+        </div>
+        <div>
+          <img></img>
+          <h3>Astrophoto</h3>
+        </div>
+        <div>
+          <img></img>
+          <h3>Property</h3>
+        </div>
+        <div>
+          <img></img>
+          <h3>Events</h3>
+        </div>
+      </div>
     </div>
   );
 };

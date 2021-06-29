1360048cb45d9f50c5f95b6eb56e0a26d3e9f4b1
jozef-wolf <wolf.joozef@gmail.com>
2021-06-29 13:44:25 +0200

    link changed to photo-gallery

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
-// import main from ".././img/main.jpg";
+import main from ".././img/main.jpg";
 
 const Home = () => {
-  return <div className="home">{/* <img src={main} alt=""></img> */}</div>;
+  return (
+    <div className="home">
+      <img src={main} alt=""></img>
+    </div>
+  );
 };
 
 export default Home;
